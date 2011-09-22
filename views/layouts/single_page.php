b7f8631fe6c77e86b634ef15bc047f7c926a50e6
Marcus Lunzenauer <mlunzena@uos.de>
2011-09-22 21:31:56 +0200

    add mustache and date JS

--- views/layouts/single_page.php
+++ views/layouts/single_page.php
@@ -8,6 +8,18 @@
     <link rel="stylesheet" href="<?= $plugin_path ?>/public/vendor/jquery.mobile/jquery.mobile-1.0b3.min.css" />
     <script src="<?= $plugin_path ?>/public/vendor/jquery/jquery-1.6.3.min.js"></script>
     <script src="<?= $plugin_path ?>/public/vendor/jquery.mobile/jquery.mobile-1.0b3.min.js"></script>
+
+
+    <script src="<?= $plugin_path ?>/public/vendor/mustache/jquery.mustache.js"></script>
+    <script src="<?= $plugin_path ?>/public/vendor/date/date.js"></script>
+
+    <script src="<?= $plugin_path ?>/public/javascripts/application.js"></script>
+
+    <script>
+      var STUDIP = STUDIP || {};
+      STUDIP.ABSOLUTE_URI_STUDIP = "<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>";
+    </script>
+
   </head>
 
 
