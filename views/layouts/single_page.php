516d1f804352349708d7d63a172c38d531b8c16c
Marcus Lunzenauer <mlunzena@uos.de>
2011-10-11 09:49:15 +0200

    update to jquery mobile 1.0rc1

--- views/layouts/single_page.php
+++ views/layouts/single_page.php
@@ -5,10 +5,10 @@
     <meta charset="windows-1252">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Stud.IP Mobile</title>
-    <link rel="stylesheet" href="<?= $plugin_path ?>/public/vendor/jquery.mobile/jquery.mobile-1.0b3.min.css" />
+    <link rel="stylesheet" href="<?= $plugin_path ?>/public/vendor/jquery.mobile/jquery.mobile-1.0rc1.min.css" />
     <link rel="stylesheet" href="<?= $plugin_path ?>/public/stylesheets/mobile.css" />
     <script src="<?= $plugin_path ?>/public/vendor/jquery/jquery-1.6.3.min.js"></script>
-    <script src="<?= $plugin_path ?>/public/vendor/jquery.mobile/jquery.mobile-1.0b3.min.js"></script>
+    <script src="<?= $plugin_path ?>/public/vendor/jquery.mobile/jquery.mobile-1.0rc1.min.js"></script>
 
 
     <script src="<?= $plugin_path ?>/public/vendor/mustache/jquery.mustache.js"></script>
