508927e18dee4cbfb21a9b4a62258ba2fc9cdea6
Marcus Lunzenauer <mlunzena@uos.de>
2011-10-11 10:10:13 +0200

    remove crust from activities

--- views/activities/index.php
+++ views/activities/index.php
@@ -4,21 +4,12 @@ $page_title = "Stud.IP - Activity Feed";
 $page_id = "activities-index";
 ?>
 
-Activities::index
-<h3>Current User: <?= $controller->currentUser()->username ?></h3>
-
-<a data-role="button" href="<?= $controller->url_for("session/destroy") ?>">logout</a>
-
-<hr style="margin: 1em; visibility: hidden;"/>
-
 <ul id="activities" data-role="listview" data-filter="true">
 </ul>
 
 <script id="template-timeline" type="text/x-mustache-tmpl">
   {{#activities}}
   <li class="activity" data-activity="{{id}}">
-    <!--
-    -->
     <img src="<?= $controller->url_for("avatars/show/{{author_id}}") ?>" alt="{{category}}" class="ui-li-icon" style="padding-top: 20px">
     <img src="<?= $plugin_path ?>/public/images/activities/{{category}}.png" alt="{{category}}" class="ui-li-icon">
     <h3><a href="#">{{title}}</a></h3>
